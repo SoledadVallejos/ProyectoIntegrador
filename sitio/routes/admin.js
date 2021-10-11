3b7cffeb6b30e01f35b439e3fe9cc3fef2cdd040
ddr2021 <dannieldavvid2004@gmail.com>
2021-10-10 23:22:42 -0300

    daniel/ Cambio formato nombre imágenes a cargar. Pongo nuevo products.json.

--- sitio/routes/admin.js
+++ sitio/routes/admin.js
@@ -2,19 +2,19 @@
 const path = require('path');
 const multer = require('multer');
 
-// CONFIG multer PARA CARGA DE ARCHIVOS (IMÁGENES EN ESTE CASO)
-// adminImage
-/* const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, '../public/img/admin-image'));
-    },
-    filename: (req, file, cb) => {
-        console.log(file); // COMPROBAR EN TERMINAL (DONDE CORRE EL PUERTO) LA DATA DE LA IMÁGEN
-        let newFilename = 'img-product-' + Date.now() + '-' + file.fieldname + '-' + path.basename(file.originalname);
-        cb(null, newFilename);
-    },
-});
-let upload = multer({ storage }); */
+n = new Date();
+//Año
+y = n.getFullYear();
+//Mes
+m = n.getMonth() + 1;
+//Día
+d = n.getDate();
+//Sec
+s = n.getMilliseconds();
+//Lo ordenas a gusto.
+let fecha = y + "-" + m + "-" + d + "-" + s;
+console.log(fecha);
+
 //SplideImages
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -22,11 +22,12 @@ const storage = multer.diskStorage({
     },
     filename: (req, file, cb) => {
         console.log(file); // COMPROBAR EN TERMINAL (DONDE CORRE EL PUERTO) LA DATA DE LA IMÁGEN
-        let newsFilesName = file.fieldname + '-' + Date.now() + '-' + path.extname(file.originalname);
+        let newsFilesName = file.fieldname + '-' + fecha + '-' + Date.now() + '-' + path.extname(file.originalname);
         cb(null, newsFilesName);
     },
 });
 let uploadMultiple = multer({ storage });
+//SplideImages/
 
 var router = express.Router();
 const { admin, add, store, edit, update, hastaLaVistaBeibi, search } = require('../controllers/adminController');
