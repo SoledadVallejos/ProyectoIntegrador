50e01a68e0028bb876a383d2f14081d42b8f5228
ddr2021 <dannieldavvid2004@gmail.com>
2021-11-15 13:44:24 -0300

    daniel/DB ACTIVO. suministra datos a vista home y vista detalle

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -1,59 +1,10 @@
-﻿/*
+﻿
+// DE A POCO IR AGREGANDO PEDAZOS DE CODIGO NUEVO PARA INCORPORAR DB
+// AGREGAS Y COMPRUEBAS DATOS OBTENIDOS DESDE DB MEDIANTE return res.send()
+// HACER COPIAS CUANDO LLEGUES A UN PUNTO DE PROGRESO FUNCIONANDO TODO !!!
 
 const db = require('../database/models');
 const { Op, Sequelize } = require('sequelize');
-
-module.exports = {
-    //********************************* COMPROBAR AQUI DATOS DE DB ESTEN LLEGANDO BIEN
-    index: (req, res) => {
-        let sectionBanner = db.Section.findAll({
-            include: [
-                'image', // TAL CUAL ESTA PUESTO EN as: 'image' EN MODELO
-                // 'section',
-                // 'category',
-                // 'feature',
-                // 'product',
-                // 'cart',
-                // 'rol'
-            ]
-        });
-        Promise.all([sectionBanner])
-            .then(([sectionBanner]) => {
-                // let productsOff = products.filter(index => {
-                //     return index.discount !== 0 || '' || null;
-                // });
-                // let productsSugest = products.filter(index => {
-                //     return index.category === 'sugest';
-                // })
-                // let bannerImages = arrObjArrImages[0].bannerImages // TIRA ARRAY DE IMÁGENES ["img1", "img2", "img3"]   TAMBIÉN PODÍA MODIFICAR EL json COMO ARRAY DE IMÁGENES DIRECTAMENTE, Y NO COMO ARRAY DE OBJETOS QUE CONTIENEN ARRAYS....
-                // // return res.send(bannerImages) //COMPROBAR
-                // console.log(sectionBanner);
-                return res.send(sectionBanner)
-                return res.render('general/index', {
-                    title: 'Roma - Venta de Indumentaria Textil',
-                    products,
-                    // productsOff,
-                    // productsSugest,
-                    // bannerImages,
-                });
-            })
-            .catch(error => console.log(error))
-        }
-        //********************************* COMPROBAR AQUI DATOS DE DB ESTEN LLEGANDO BIEN /
-    }
-    
-    */
-
-
-
-
-const db = require('../database/models');
-const { Op, Sequelize } = require('sequelize');
-
-// const fs = require('fs');
-// const path = require('path');
-// let products = JSON.parse(fs.readFileSync(path.join(__dirname, '../data', 'products.json'), 'utf-8'));
-// let arrObjArrImages = JSON.parse(fs.readFileSync(path.join(__dirname, '../data', 'bannerSplide.json'), 'utf-8'));
 
 module.exports = {
     index: (req, res) => {
@@ -68,24 +19,22 @@ module.exports = {
             ]
         });
         let productDiscount = db.Product.findAll({
-            // attributes: {  exclude: ['id'] }, // FUNCIONA EXCLUYE LOS INDICADOS
-            // attributes: ['name'], FUNCIONA EXCLUYE TODOS MENOS LOS INDICADOS
             where: { // OBTENER UN ARRAY DE OBJETOS MUY SIMILAR A ESTRUCTURA JSON ORIGINAL
                 discount: {
                     [Op.gte]: 5,
                 },
             },
             include: [{ //MAS CONVENIENTE
-                raw: true,
+                raw: true, // AQUI SI ES UTIL. NO BORRARLO
                 association: 'image',
                 attributes: ['file'],
-
             }],
             // raw: true, // FUNCIONA. ARRAY PLANO
             // nest: true // FUNCIONA. MANTIENE EL OBJETO ANIDADO
+            // attributes: {  exclude: ['id'] }, // FUNCIONA EXCLUYE LOS INDICADOS
+            // attributes: ['name'], FUNCIONA EXCLUYE TODOS MENOS LOS INDICADOS
         })
         let productCategory = db.Category.findAll({
-            // raw: true,
             include: [{
                 association: 'image',
                 attributes: ['file'],
@@ -104,55 +53,20 @@ module.exports = {
                 let productsOff = productDiscount.map(index => {
                     return index
                 });
-                // return res.send(productCategory) //COMPROBAR
-                // return res.send(bannerImages) //COMPROBAR
-                // return res.send(productsOff) //COMPROBAR
+                // return res.send(productCategory) //COMPROBAR ANTES DE PROSEGUIR
+                // return res.send(bannerImages) //COMPROBAR ANTES DE PROSEGUIR
+                // return res.send(productsOff) //COMPROBAR ANTES DE PROSEGUIR
+                // TRANSICION JSON A DB: 1_RUTAS ACTUALIZADAS EN INDEX 2_LLEGUE DE DB DATOS CORRECTOS (igual o parecido como llegaba del JSON) 3_EXISTA ARCHIVO ENLAZADO (imagen en este caso)
                 return res.render('general/index', {
                     title: 'Roma - Venta de Indumentaria Textil',
-                    bannerImages, // LISTO. DB EN ACCION
-                    productCategory,
-                    productsOff, // TRANSICION JSON A DB: 1_RUTAS ACTUALIZADAS EN INDEX 2_LLEGUE DE DB DATOS CORRECTOS (igual o parecido como llegaba del JSON) 3_EXISTA ARCHIVO ENLAZADO (imagen en este caso)
+                    bannerImages, // LISTO. DB EN ACCION QUE EMOCION!!
+                    productCategory, // LISTO PARA MOSTRAR DATOS DE HOMBRE O MUJER...
+                    productsOff, //LISTO. MUESTRA PRODUCTOS SOLO CON DESCUENTO EN HOME
                     // productsSugest, NUNCA SE USÓ
                     // products, // NUNCA SE USÓ
                 });
             })
             .catch(error => console.log(error))
-
-
-
     },
 }
 
-
-
-
-
-
-
-
-
-
-
-
-// let sectionBanner = db.Section.findAll({
-//     include: [
-//         'image' // TAL CUAL ESTA PUESTO EN as: 'image' EN MODELO
-//     ]
-// });
-// Promise.all([sectionBanner])
-//     .then(([sectionBanner]) => {
-//         return res.send(sectionBanner)
-//         return res.render('general/index', {
-//             title: 'Roma - Venta de Indumentaria Textil',
-//             products,
-//             // productsOff,
-//             // productsSugest,
-//             // bannerImages,
-//         });
-//     })
-//     .catch(error => console.log(error))
-
-
-
-
-    // ****************************************************** VERSION JSON **************************************************************
--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,20 +1,41 @@
-const fs = require('fs');
-const path = require('path');
-let products = JSON.parse(fs.readFileSync(path.join(__dirname, '../data', 'products.json'), 'utf-8'))
+const db = require('../database/models');
+const { Op, Sequelize } = require('sequelize');
+
+// ELIMINAR O COMENTAR UNA VEZ RESUELTO CONSUMO DATOS DE DE DB 
+// const fs = require('fs');
+// const path = require('path');
+// let products = JSON.parse(fs.readFileSync(path.join(__dirname, '../data', 'products.json'), 'utf-8'))
 
 module.exports = {
     productDetail: (req, res) => {
-        let id = +req.params.id;
-        let product = products[id]; // PRODUCTO ESPECÍFICO DINAMICO
-        let productImg = products[id].splideImages; // IMAGENES DE PRODUCTO ESPECÍFICO DINÁMICO
-        // res.send(productImg); // COMPROBAR PRODUCTO ESPECÍFICO DINAMICO
-        return res.render('products/productDetail', {
-            title: 'Detalle del Producto',
-            id,
-            products,
-            product,
-            productImg,
-        });
+        let products = db.Product.findAll({
+            include: [{
+                association: 'image',
+                attributes: ['file'],
+            }],
+        })
+        Promise.all([products])
+            .then(([products]) => {
+                //----------------------CAMBIAR SUMINISTRO DE DATOS DE JSON A DB  UNO A UNO POCO A POCO 
+                let id = +req.params.id - 1; // PONGO -1 PARA CORREGIR INCOINCIDENCIA ENTRE id Y GRUPOD DE IMAGENES DEL SLIDE EN  productDetail.ejs
+                //----------------------COMPROBAR EN productImg LLEGUE DE DB DATOS MUY SIMILARES COMO LLEGABA DEL JSON
+                // EXTREAER IMAGENES DE DB Y PONERLO EN productImg PARA CONSUMO DE  SLIDE EN  productDetail.ejs
+                let productImgsById = products[id].image; // IMAGENES DE PRODUCTO ESPECÍFICO DINÁMICO SEGUN EL Iid PUESTO EN URI
+                let productImg = productImgsById.map(element => {
+                    return element.file
+                })
+                // return res.send(productImg); // COMPROBAR DATOS ANTES!
+                // return res.send(product); // COMPROBAR DATOS ANTES! 
+                // return res.send(productImg); // COMPROBAR DATOS ANTES!
+                // return res.send(products)// COMPROBAR DATOS ANTES!
+                return res.render('products/productDetail', {
+                    title: 'Detalle del Producto',
+                    id, //BIEN... PARA MOSTRAR PRODUCTO SEGUN EL id DEL URI
+                    products, // BIEN... PARA MOSTRAR DETALLES DEL PRODUCTO
+                    productImg, //BIEN... PARA EL SLIDE DEL DETALLE
+                });
+            })
+            .catch(error => console.log(error))
     },
     cart: (req, res) => {
         return res.render('products/cart', {
